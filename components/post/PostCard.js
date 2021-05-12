e5e5511731ecee100645c3ec10bff6bdc476e883
leedoopal <1233wsh@gmail.com>
2021-05-12 17:14:03 +0900

    게시글 - like, comment hook 추가

--- components/post/PostCard.js
+++ components/post/PostCard.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import { useRecoilValue } from "recoil";
 
 import { Card, Popover, Button, Avatar } from "antd";
 import {
   RetweetOutlined,
+  HeartTwoTone,
   HeartOutlined,
   MessageOutlined,
   EllipsisOutlined
@@ -14,16 +15,26 @@ import { userMe } from "../../stores/user";
 import PostImage from "../post/PostImage";
 
 const PostCard = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState('');
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, [liked]);
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, [commentFormOpened]);
   const id = useRecoilValue(userMe)?.id;
-  console.log('id: ', id);
 
   return (
     <div>
       <Card cover={post.images && <PostImage images={post.images} />}
             actions={[
               <RetweetOutlined key="retweet" />,
-              <HeartOutlined key="heart" />,
-              <MessageOutlined key="comment" />,
+              liked ? <HeartTwoTone twoToneColor="eb2f96" key="heartTwo"
+                                    onClick={onToggleLike} /> :
+                <HeartOutlined key="heart" onClick={onToggleLike} />
+              ,
+              <MessageOutlined key="comment" onClick={onToggleComment} />,
               <Popover key="more" content={(
                 <Button.Group>
                   {post.user.id === id ? (
@@ -40,6 +51,11 @@ const PostCard = ({ post }) => {
         <Card.Meta avatar={<Avatar>{post.user.nickname[0]}</Avatar>}
                    title={post.user.nickname} description={post.content} />
       </Card>
+      {commentFormOpened && (
+        <div>
+          댓글 넣을 곳
+        </div>
+      )}
     </div>
   )
 }
